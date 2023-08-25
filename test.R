08557c600d024e4f430d8a7700439a05fad10f57
jiyekwon <61208452+jiyekwon@users.noreply.github.com>
2023-08-24 23:18:10 -0400

    added data folder and updated test.R file.

--- test.R
+++ test.R
@@ -1,1 +1,8 @@
 # https://resources.github.com/github-and-rstudio/
+
+# 1. start with reading TCV CEA R code What you need to know.docx
+# 2. 'TCEA_INPUT.R' 
+
+# 3. add all the helper functions in one folder 
+
+
